04e6957f6b31eb8f683f26a30d3e33e5c46c1aba
Aindri Ghoshal <75011539+aindri7@users.noreply.github.com>
2025-03-15 18:41:36 +0530

    Delete Angular-Product-Inventory-Management directory

--- Angular-Product-Inventory-Management/src/app/shared/models/user.model.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    id: number;
-    email: string;
-    password: string;
-    firstName: string;
-    lastName: string;
-    location: string;
-    mobileNumber: string;
-}
